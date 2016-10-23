1379013b5a7abaffeba09ec4af5ba778c9ef3a75
Jos Spencer <crazyandroid101@gmail.com>
2016-10-24 03:35:47 +0800

    Added a few new things
    Added @user response, a rate limit and file upload for zerg, regex on a message and descriptions

--- sapphire.rb
+++ sapphire.rb
@@ -1,12 +1,28 @@
+#Bot Setup + Connect
+#-----------------------
 require 'discordrb'
 token = File.read("token.txt")
-puts token
-bot = Discordrb::Bot.new token: token, client_id: 239802618757644288#, prefix: '.',
+bot = Discordrb::Commands::CommandBot.new token: token, client_id: 239802618757644288, prefix: '.'
+
+#Buckets
+#-----------------------
+bot.bucket :zerg, limit: 2, time_span: 60, delay: 15
 
 
-bot.message(with_text: ".ping") do |event|
-  event.respond "Pong o/ #{event.user.name}!"
+#Commands
+#-----------------------
+bot.command( :ping, description: "Alive check for the bot", help_available: true) do |event|
+    "Pong o/ #{event.user.name}!"
 end
 
+bot.command( :zerg, bucket: :zerg, description: "Posts a cute zergling gif", help_available: true) do |event|
+    event.channel.send_file File.new("images/zerg.gif")
+end
+
+#Non-Commands
+#----------------------
+bot.message(contains: /(sapphire)/i) do |event|
+  event.channel.send_message "Do you need me #{event.user.mention}?"
+end
 
 bot.run
