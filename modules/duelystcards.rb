b730decf60ca4cbb6d510ef7063b4051dc5dccf4
Jos Spencer <crazyandroid101@gmail.com>
2016-11-06 23:53:06 +0800

    Fixed code to work as module

--- modules/duelystcards.rb
+++ modules/duelystcards.rb
@@ -1,9 +1,9 @@
 module Bot
   module DiscordCommands
-    module Rawr
+    module DuelystCards
       extend Discordrb::Commands::CommandContainer
       extend Discordrb::EventContainer
-      bot.command(:card, help_available: false) do |event, *cardname|
+      command(:card, help_available: false) do |event, *cardname|
         cardinfo = open("https://duelyststats.info/scripts/carddata/get.php?cardName=#{cardname.join('%20')}")
         event.respond cardinfo.read
       end
