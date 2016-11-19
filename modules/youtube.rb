3cfaff5817c08d7a2f4527d3bae2177726c3fe8b
Jos Spencer <crazyandroid101@gmail.com>
2016-11-19 17:49:25 +0800

    Cleaned and Beautified

--- modules/youtube.rb
+++ modules/youtube.rb
@@ -18,24 +18,24 @@ module Bot
           response = JSON.parse(apijson.read)
           event.respond 'Nothing found :scream:' if response.empty?
           break if response.empty?
-          songlink = "#{response['items'][0]['id']['videoId']}"
+          songlink = (response['items'][0]['id']['videoId']).to_s
         end
-          unless event.voice.nil?
-            event.respond 'Already playing music'
-            break
-          end
-          channel = event.user.voice_channel
-          unless channel.nil?
-            voice_bot = event.bot.voice_connect(channel)
-            event.send_temp('Downloading...', 7)
-            system("youtube-dl --no-playlist --max-filesize 50m -o 'music/#{event.server.id}.%(ext)s' -x --audio-format mp3 #{songlink}")
-            event.respond 'Playing'
-            voice_bot.play_file("./music/#{event.server.id}.mp3")
-            voice_bot.destroy
-            break
-          end
-          event.respond "You're not in any voice channel!"
+        unless event.voice.nil?
+          event.respond 'Already playing music'
+          break
         end
+        channel = event.user.voice_channel
+        unless channel.nil?
+          voice_bot = event.bot.voice_connect(channel)
+          event.send_temp('Downloading...', 7)
+          system("youtube-dl --no-playlist --max-filesize 50m -o 'music/#{event.server.id}.%(ext)s' -x --audio-format mp3 #{songlink}")
+          event.respond 'Playing'
+          voice_bot.play_file("./music/#{event.server.id}.mp3")
+          voice_bot.destroy
+          break
+        end
+        event.respond "You're not in any voice channel!"
+      end
       command(:stop, description: stop_desc) do |event|
         event.voice.stop_playing
         return nil
