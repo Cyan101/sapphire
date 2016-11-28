d920de8d277e631267cdd73685d2d469ca9f869a
Jos Spencer <crazyandroid101@gmail.com>
2016-11-28 16:45:24 +0800

    Removed un-used variables and renamed some fields

--- modules/embeds.rb
+++ modules/embeds.rb
@@ -16,8 +16,6 @@ module Bot
           (x.system + x.user).to_f / (x.user + x.system + x.idle).to_f
         end
         all_cpus = cpus.map.with_index { |x, y| "**#{y + 1}.** #{x.to_s[0..3]}%" }
-        cpu_usage = (cpus.reduce(:+).to_f / cpus.length).to_s[0..3]
-        time_taken = sys.at.utc
         event.channel.send_embed do |e|
           e.author = {name: event.bot.profile.name, url: 'http://github.com/cyan101/sapphire', icon_url: event.bot.profile.avatar_url}
           e.color = '3498db'
@@ -30,8 +28,8 @@ module Bot
           # e.footer      = { text: '- Created by Cyan', icon_url: event.bot.profile.avatar_url }
           e.add_field name: 'CPU Cores Usage:', value: all_cpus.join("\n"), inline: true
           e.add_field name: 'Servers/Users:', value: "**Servers:** #{event.bot.servers.count}\n**Users:** #{event.bot.users.count}", inline: true
-          e.add_field name: 'Free System Memory: ', value: "`#{free_mem[0..3]}GB`", inline: true
-          e.add_field name: 'In-Use Memory:', value: "`#{res_mem[0..4]}MB`", inline: true
+          e.add_field name: 'Free Server Memory: ', value: "`#{free_mem[0..3]}GB`", inline: true
+          e.add_field name: "#{event.bot.profile.name} is using:", value: "`#{res_mem[0..4]}MB`", inline: true
           e.add_field name: 'Invite Link', value: "[Click here to Invite #{event.bot.profile.name} to your server!](#{event.bot.invite_url})", inline: false
         end
       end
