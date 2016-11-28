84f5a4a411897224d90a6c6c763a6ead7c9672df
Jos Spencer <crazyandroid101@gmail.com>
2016-11-28 17:32:43 +0800

    Added 'trash' and 'weout' command

--- modules/pictures.rb
+++ modules/pictures.rb
@@ -6,13 +6,27 @@ module Bot
       extend Discordrb::Commands::CommandContainer
 
       bucket :pictures, limit: 3, time_span: 60, delay: 20
+      bucket :trash, limit: 1, time_span: 60
 
-      picscooldown = 'Please wait %time%s before asking for more cute pics'
+      picscooldown = 'Please wait %time%s before asking for more pics'
+      trash_cooldown = 'Please stop being mean to people for just %time%s'
       zerg_desc = 'Posts a cute zergling gif'
       cat_desc = 'Posts a random cat'
+      weout_desc = 'Shows everyone that you are out of here!'
+      trash_desc = 'Insults someone :T'
 
       command(:zerg, bucket: :pictures, description: zerg_desc, rate_limit_message: picscooldown) do |event|
         event.channel.send_file File.new('images/zerg.gif')
+      end
+      
+      command(:trash, bucket: :trash, description: trash_desc, rate_limit_message: trash_cooldown) do |event|
+        next event.respond('I need the `bully` role to use this command') unless event.bot.profile.on(event.server).roles.map {|x| x.name }.join.include? 'bully'
+        event.channel.send_file File.new('images/trash_mean.gif')
+      end
+      
+      command([:weout, :outofhere, :weareoutofhere, :fuckthis],
+              bucket: :pictures, description: weout_desc, rate_limit_message: picscooldown) do |event|
+        event.channel.send_file File.new('images/weoutofhere.gif')
       end
 
       command([:cat, :kitten, :pussy], bucket: :pictures, description: cat_desc, rate_limit_message: picscooldown) do |_event|
